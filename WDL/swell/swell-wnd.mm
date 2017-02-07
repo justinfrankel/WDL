justinfrankel/licecap

commit ce641d4f5b0de0b20daf254998b55a0ec1a20c93

avoid drawing swell listview cell icon over the cell boundary -- from 4592cf3e

WDL/swell/swell-wnd.mm
@@ -251,7 +251,8 @@ STANDARD_CONTROL_NEEDSDISPLAY_IMPL
   if (status)
   {
 //    [controlView lockFocus];
-    [status drawInRect:NSMakeRect(cellFrame.origin.x,cellFrame.origin.y,cellFrame.size.height,cellFrame.size.height) fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+    int w=min(cellFrame.size.width, cellFrame.size.height);
+    [status drawInRect:NSMakeRect(cellFrame.origin.x,cellFrame.origin.y,w,cellFrame.size.height) fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
  //   [controlView unlockFocus];
   }
   cellFrame.origin.x += cellFrame.size.height + 2.0;

justinfrankel/WDL

commit bf4bfcdd94dff3306457d932cb2b08ef889eece2

SWELL: fix build with WDL_NO_DEFINE_MINMAX defined
Building with WDL_NO_DEFINE_MINMAX defined on macOS is broken since ce641d4f5b0de0b20daf254998b55a0ec1a20c93
because it tries to use the `min` macro which does not exists.

This commit fixes this by using wdl_min instead.

WDL/swell/swell-wnd.mm
@@ -251,7 +251,7 @@ STANDARD_CONTROL_NEEDSDISPLAY_IMPL
   if (status)
   {
 //    [controlView lockFocus];
-    int w=min(cellFrame.size.width, cellFrame.size.height);
+    int w=wdl_min(cellFrame.size.width, cellFrame.size.height);
     [status drawInRect:NSMakeRect(cellFrame.origin.x,cellFrame.origin.y,w,cellFrame.size.height) fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
  //   [controlView unlockFocus];
   }