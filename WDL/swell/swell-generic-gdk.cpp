67b8f4a10504948e8a94daed58cc8dabc1e00a77
Marcus Gursch <marcus.gursch@rub.de>
2022-02-01 00:31:02 +0100

    swell: work around kwin window button glitch

--- WDL/swell/swell-generic-gdk.cpp
+++ WDL/swell/swell-generic-gdk.cpp
@@ -314,7 +314,8 @@ void swell_recalcMinMaxInfo(HWND hwnd)
   h.max_height= mmi.ptMaxSize.y;
   h.min_width= mmi.ptMinTrackSize.x;
   h.min_height= mmi.ptMinTrackSize.y;
-  gdk_window_set_geometry_hints(hwnd->m_oswindow,&h,(GdkWindowHints) (GDK_HINT_POS | GDK_HINT_MIN_SIZE | GDK_HINT_MAX_SIZE));
+  gdk_window_set_geometry_hints(hwnd->m_oswindow,&h,(GdkWindowHints) (GDK_HINT_POS | GDK_HINT_MIN_SIZE));
+  gdk_window_set_geometry_hints(hwnd->m_oswindow,&h,(GdkWindowHints) (GDK_HINT_POS | GDK_HINT_MAX_SIZE)); // workaround kwin window button glitch
 }
 
 void SWELL_initargs(int *argc, char ***argv) 
