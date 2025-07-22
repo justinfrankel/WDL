16a96a9e1599b371f0b33fa67aa8520f4f566011
Tee <i@lado.me>
2025-07-23 03:48:49 +0800

    feat: add keyboard handling for IM

--- WDL/swell/swell-generic-gdk.cpp
+++ WDL/swell/swell-generic-gdk.cpp
@@ -44,6 +44,9 @@ extern "C" {
 #include "../wdlcstring.h"
 #include "../wdlutf8.h"
 
+#ifdef SWELL_SUPPORT_IM
+#include "swell-im.h"
+#endif
 
 #if !defined(SWELL_TARGET_GDK_NO_CURSOR_HACK)
   #define SWELL_TARGET_GDK_CURSORHACK
@@ -1349,6 +1352,22 @@ static void OnKeyEvent(GdkEventKey *k)
   MSG msg = { hwnd, msgtype, kv, modifiers, };
   INT_PTR extra_flags = 0;
   if (DialogBoxIsActive()) extra_flags |= 1;
+#ifdef SWELL_SUPPORT_IM
+  // before SWELLAppMain TCP and MCP need it
+  if (im_hwnd_match(hwnd, "Edit") || im_hwnd_match(hwnd, "combobox"))
+  {
+    if (GetProp(hwnd, "IM_CONTEXT"))
+      if (gtk_im_context_filter_keypress((GtkIMContext *)GetProp(hwnd, "IM_CONTEXT"), k))
+      {
+        if (GetProp(hwnd, "IM_PREEDIT_STATE")) {
+          if (k->keyval == DEF_GKY(Left)) {
+          } else if (k->keyval == DEF_GKY(Right)) {
+          }
+        }
+        return;
+      }
+  }
+#endif
   if (SWELLAppMain(SWELLAPP_PROCESSMESSAGE,(INT_PTR)&msg,extra_flags)<=0)
     SendMessage(hwnd, msg.message, kv, modifiers);
 }
